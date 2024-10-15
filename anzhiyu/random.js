2e8bf1363a55cb1f7d3de441d80b029375889f47
lxb-blog <927786227@qq.com>
2024-10-15 16:41:07 +0800

    Site updated: 2024-10-15 16:41:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/13/GitHub+hexo/","2024/10/13/hexo/","2024/10/13/hexo文章发布模板/","2024/10/13/hexo之安装主题/","2024/10/13/hexo美化之一图流/","2024/10/13/hexo美化之信封留言板/"];function toRandomPost(){
+var posts=["posts/undefined/","posts/undefined/","posts/undefined/","posts/undefined/","posts/undefined/","posts/undefined/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
