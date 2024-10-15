dfa5e7fde3ebf272587c76390f9d7a3be5f953e4
lxb-blog <927786227@qq.com>
2024-10-15 17:09:41 +0800

    Site updated: 2024-10-15 17:09:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/undefined/","posts/undefined/","posts/undefined/","posts/undefined/","posts/undefined/","posts/undefined/"];function toRandomPost(){
+var posts=["posts/93c315da/","posts/21600/","posts/51ff7f31/","posts/dbdf5b2f/","posts/93c315da/","posts/5a3686e8/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
