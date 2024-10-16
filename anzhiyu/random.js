a4fc5d0409fb52d2ab9bcb23e69885c66772549f
lxb-blog <97530887+lxb-blog@users.noreply.github.com>
2024-10-16 02:43:20 +0000

    Create blank.yml Updated By Github Actions

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["posts/93c315da/","posts/21600/","posts/51ff7f31/","posts/dbdf5b2f/","posts/93c315da/","posts/5a3686e8/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
